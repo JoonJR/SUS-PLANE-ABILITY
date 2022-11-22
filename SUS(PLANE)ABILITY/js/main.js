6a81d46d483da448156eae00b6b03de19066e533
Joona Rantala <joonajani.rantala@gmail.com>
2022-11-22 15:44:27 +0200

    s

--- SUS(PLANE)ABILITY/js/main.js
+++ SUS(PLANE)ABILITY/js/main.js
@@ -12,7 +12,8 @@ map.setView([60, 24], 7);
 // global variables
 
 // icons
-
+const blueIcon = L.divIcon({className: 'blue-icon'})
+const greenIcon = L.divIcon({className: 'green-icon'})
 
 // form for player name
 
@@ -25,13 +26,25 @@ async function getData(url){
     return data;
 }
 // function to update game status
-
+function updateStatus(status) {
+  document.querySelector('#consumed').innerHTML = status.co2.consumed;
+  document.querySelector('#countries').innerHTML = status.countries;
+  document.querySelector('#dice').innerHTML = status.dice;
+}
 // function to show weather at selected airport
+function showWeather(airport){
 
+  document.querySelector('#temperature').innerHTML =  `${airport.weather.temp}°C`;
+  document.querySelector('#weather-icon').src = airport.weather.icon;
+
+
+}
 // function to check if any goals have been reached
 
 // function to update goal data and goal table in UI
+function updateGoals(goals){
 
+}
 // function to check if game is over
 
 // function to set up game
@@ -41,11 +54,30 @@ async function gameSetup(){
   try {
     const gameData = await getData('testdata/newgame.json');
     console.log(gameData);
+    updateStatus(gameData.status);
+
     for(let airport of gameData.location){
       const marker = L.marker([airport.latitude, airport.longitude]).addTo(map);
-      if(airport.active === true){
+      if(airport.active){
+        showWeather(airport);
         marker.bindPopup(`You are here: <b>${airport.name}</b>`);
         marker.openPopup();
+        marker.setIcon(greenIcon);
+      }
+      else {
+        marker.setIcon(blueIcon);
+        const popupContent = document.createElement('div');
+        const h4 = document.createElement('h4');
+        h4.innerHTML = airport.name;
+        popupContent.append(h4);
+        const goButton = document.createElement('button');
+        goButton.classList.add('button');
+        goButton.innerHTML = 'Fly here';
+        popupContent.append(goButton);
+        const p = document.createElement('p');
+        p.innerHTML = `Distance ${airport.distance} km`;
+        popupContent.append(p);
+        marker.bindPopup(popupContent);
       }
     }
   } catch (error){
